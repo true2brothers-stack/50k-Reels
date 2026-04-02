a2e33678649f362b38c94d1a2aeb4989f34dd859
TrueBrothers (via WeWeb) <true2brothers@gmail.com>
2026-04-03 00:49:16 +0100

    v6 - final 2.0

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 5;
+const version = 6;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
